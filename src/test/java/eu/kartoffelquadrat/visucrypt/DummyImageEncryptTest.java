cc2cb2cf5bae69ae873391e9999ffc041076d0ff
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2022-09-20 11:02:57 -0400

    midified test to verify creation of test share files

--- src/test/java/eu/kartoffelquadrat/visucrypt/DummyImageEncryptTest.java
+++ src/test/java/eu/kartoffelquadrat/visucrypt/DummyImageEncryptTest.java
@@ -8,22 +8,34 @@
 
 package eu.kartoffelquadrat.visucrypt;
 
+import java.io.File;
 import java.io.IOException;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class DummyImageEncryptTest {
 
   @Test
   public void testUpdateStock() throws IOException {
+    final String targetDir = "/tmp";
+
     Launcher
-        .main(new String[] {"/Users/schieder/Code/VisuCrypt/src/test/resources/test-image.png", "/tmp"});
+        .main(new String[] {"/Users/schieder/Code/VisuCrypt/src/test/resources/test-image.png",
+            targetDir});
 
-    // TODO: also verify the outcome images make sense. Parse them again, verify their superposition
-    //  bool values.
+    // Verify output images exist in /tmp directory (expected target location)
+    File share1File = new File(targetDir + "/share1.png");
+    File share2File = new File(targetDir + "/share2.png");
+    Assert.assertTrue("Expected a test share at /tmp/share1.png, but the file does not exist.",
+        share1File.exists());
+    Assert.assertTrue("Expected a test share at /tmp/share2.png, but the file does not exist.",
+        share2File.exists());
 
+    // TODO: Actually inspect produces files to see if they correspond to the expected outcome.
 
     // finally clean up generated test shares
-
+    share1File.delete();
+    share2File.delete();
   }
 
 }
